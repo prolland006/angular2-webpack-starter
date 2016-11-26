730f31cf0e6ff6f153bdb7ae86d2b91b123ce2c0
Prolland006 <rolland.patrice@gmail.com>
2016-11-26 13:50:34 +0100

    stuff

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 3000;
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = webpackMerge(commonConfig({env: ENV}).metadata, {
   host: HOST,
-  port: PORT,
+  port: 8080,
   ENV: ENV,
   HMR: HMR
 });
